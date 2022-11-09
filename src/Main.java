432a2aab7bf6dfaea86f3395a7b4ac6b9df6a62d
Trolex221 <polloryanxd@gmail.com>
2022-11-09 20:30:13 +0100

    prueba

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         //Create the proxies
         ActorProxy proxy = ActorContext.spawnActor(new Actor("Oscar"));
         ActorProxy proxy2 = ActorContext.spawnActor(new Actor("Javi"));
-        //ActorProxy decorator = ActorContext.spawnActor(new FirewallDecorator(new Actor("Prueba")));
+        ActorProxy decorator = ActorContext.spawnActor(new FirewallDecorator(new Actor("Prueba")));
 
         //Starting the communication
         proxy.send(new Message(proxy2.getActor(), "Start the communication"));
